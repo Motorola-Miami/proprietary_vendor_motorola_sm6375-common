3fe7c41e26ba96786a70c791796b3318f6dc2d89
Julian Veit <Claymore1298@gmail.com>
2024-11-06 22:12:08 +0100

    sm6375-common: add libskewknob.so
    11-05 16:52:41.475   952  1031 D linker  : dlerror set to "dlopen failed: library "libskewknob.so" not found"
    
    Change-Id: I35a025ce0af5848e1861f14473d342301cf4f759

--- sm6375-common-vendor.mk
+++ sm6375-common-vendor.mk
@@ -908,6 +908,7 @@ PRODUCT_COPY_FILES += \
     vendor/motorola/sm6375-common/proprietary/vendor/lib64/librilqmimiscservices.so:$(TARGET_COPY_OUT_VENDOR)/lib64/librilqmimiscservices.so \
     vendor/motorola/sm6375-common/proprietary/vendor/lib64/librilqmiservices.so:$(TARGET_COPY_OUT_VENDOR)/lib64/librilqmiservices.so \
     vendor/motorola/sm6375-common/proprietary/vendor/lib64/librpmb.so:$(TARGET_COPY_OUT_VENDOR)/lib64/librpmb.so \
+    vendor/motorola/sm6375-common/proprietary/vendor/lib64/libskewknob.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libskewknob.so \
     vendor/motorola/sm6375-common/proprietary/vendor/lib64/libscveCommon.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libscveCommon.so \
     vendor/motorola/sm6375-common/proprietary/vendor/lib64/libscveCommon_stub.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libscveCommon_stub.so \
     vendor/motorola/sm6375-common/proprietary/vendor/lib64/libscveObjectSegmentation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libscveObjectSegmentation.so \
